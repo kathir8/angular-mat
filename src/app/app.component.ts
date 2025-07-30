ef56dcfc2046ff6a69c7c215ba77049f2c12f73d
Kathiravan <sukn99@gmail.com>
2025-07-30 14:04:53 +0530

    angular mat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTabsModule } from '@angular/material/tabs';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MatButtonModule, MatInputModule, MatSelectModule, NgFor,MatTabsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-mat';
+  foods = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'},
+  ];
+  servers = [
+    {
+      ServerID:1,
+      ServerDesc:'Dev',
+    },
+    {
+      ServerID:2,
+      ServerDesc:'Alpha',
+    },
+  ]
 }
